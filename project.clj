fdc87f3b65fbef534818aea478816306bec274a5
Tobias Bayer <tobayer@googlemail.com>
2015-06-30 12:41:51 +0200

    Include lein-midje plugin

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject midje-intro "0.0.1-SNAPSHOT"
   :description "Some sample code for my Midje introduction talk at :clojureD 2015."
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
 
